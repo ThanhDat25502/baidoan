9ce847c7600205d1a8fb4d7488b3cf22d06c8a5d
ThanhDat2006 <thanhdat25502@gmail.comgit config --global user.email thanhdat25502@gmail.com>
2021-06-08 13:59:39 +0700

    first commit

--- public/script.js
+++ public/script.js
@@ -1,31 +1,38 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.muted = true;
+const myVideo = document.createElement('video');    // phương thức createElement() tạo ra phần tử HTML được chỉ định bởi tagName,
+myVideo.muted = true;     //tắt tiếng video từ myVideo để nó không phát lại
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
   port: '443'
-});
+}); //tạo một kết nối ngang hàng có id duy nhất
+
+myPeer.on('open', id => {   //yêu cầu kết nối ngang hàng
+  socket.emit('join-room', ROOM_ID, id)     // hiển thị địa chỉ cho bất kì ai tham gia phong
+})
+
+
 let myVideoStream;
 const peers = {}
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getUserMedia({    //yêu cầu quyền truy cập video và âm thanh
   video: true,
   audio: true
-}).then(stream => {
+}).then(stream => {        // then: tạo luồng cho video
   myVideoStream = stream;
-  addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, stream)     //thêm một luồng video mới vào tệp myVideo.
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
+    call.on('stream', userVideoStream => {  //kết nối luồng video từ clinet khác
       addVideoStream(video, userVideoStream)
     })
-  })
+  })  
 
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
-  })
+  })//  Trong khối mã này, khi người dùng khác tham gia vào phòng với cùng một id phòng thì nó sẽ hiển thị luồng của anh ấy đến cửa sổ của tôi
+
   // input value
   let text = $("input");
   // when press enter send message
@@ -39,43 +46,41 @@ navigator.mediaDevices.getUserMedia({
     $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
     scrollToBottom()
   })
-})
+});
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
 
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
+  const call = myPeer.call(userId, stream)    //kết nối cuộc gọi đến của client từ địa chỉ id gủi cho client
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
-  })
+  })    //add luồng video vào web của minh
   call.on('close', () => {
-    video.remove()
+    video.remove()    //xóa luồng video và kết thúc cuộc gọi
   })
 
   peers[userId] = call
 }
 
-function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
-}
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})    // người dùng ngắt kết nối thì đóng cuộc gọi
 
+
+
+function addVideoStream(video, stream) {
+  video  = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  })      //addEventListener () gắn một trình xử lý sự kiện vào phần tử được chỉ định.
+  videoGrid.append(video);    //append() Chèn nội dung, di chuyển thành phần vào trong thành phần khác,chèn video vào videoGird hàng 36 room.js
 
 
 const scrollToBottom = () => {
   var d = $('.main__chat_window');
   d.scrollTop(d.prop("scrollHeight"));
-}
+} //tạo cửa sổ chat
 
 
 const muteUnmute = () => {
@@ -87,7 +92,7 @@ const muteUnmute = () => {
     setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
   }
-}
+} // chức năng bật tắt tiếng
 
 const playStop = () => {
   console.log('object')
@@ -99,7 +104,7 @@ const playStop = () => {
     setStopVideo()
     myVideoStream.getVideoTracks()[0].enabled = true;
   }
-}
+}// chức năng dừng video
 
 const setMuteButton = () => {
   const html = `
@@ -107,7 +112,7 @@ const setMuteButton = () => {
     <span>Mute</span>
   `
   document.querySelector('.main__mute_button').innerHTML = html;
-}
+}//tao icon âm thanh khi bật
 
 const setUnmuteButton = () => {
   const html = `
@@ -115,7 +120,7 @@ const setUnmuteButton = () => {
     <span>Unmute</span>
   `
   document.querySelector('.main__mute_button').innerHTML = html;
-}
+}//tao icon âm thanh khi tắt
 
 const setStopVideo = () => {
   const html = `
@@ -123,12 +128,14 @@ const setStopVideo = () => {
     <span>Stop Video</span>
   `
   document.querySelector('.main__video_button').innerHTML = html;
-}
+}//tạo icon khi bật video
 
 const setPlayVideo = () => {
   const html = `
   <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
-  `
+  `;
   document.querySelector('.main__video_button').innerHTML = html;
-}
+};//tạo icon khi tắt video
+
+
--- server.js
+++ server.js
@@ -1,42 +1,45 @@
-const express = require('express')
-const app = express()
-// const cors = require('cors')
-// app.use(cors())
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const express = require('express')    //gọi thư viện express
+const app = express()   //tạo đối tượng có kiểu express()
+const server = require('http').Server(app)    //tạo một máy chủ để sử dụng trong socket.io
+const io = require('socket.io')(server)   //kết nối sever vs soket.io
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
-});
-const { v4: uuidV4 } = require('uuid')
+});   // tạo máy chủ ngang hàng
+app.use('/peerjs', peerServer);     // truy cập vào máy chủ ngang hàng.
 
-app.use('/peerjs', peerServer);
+const { v4: uuidV4 } = require('uuid')    //tạo địa chỉ uuidv4 và yêu cầu đưa uuid vào dự án
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs')   //view engine: chuyển đổi tu ejs thành tệp HTML được gửi đến client
+app.use(express.static('public'))   // sử dụng Express cung cấp các tệp tĩnh như hình ảnh, tệp CSS và tệp JavaScript trong thư mục pulic
+                                    //thiết lập thư mục chung làm thư mục tĩnh
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {    //tạo một căn phòng hoàn toàn mới,
   res.redirect(`/${uuidV4()}`)
-})
+})    //tra loi diaa chi uuid khi get yeu cau
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
-})
+})    //lay roomid để chuyển đến url 
 
-io.on('connection', socket => {
+io.on('connection', socket => {   // tạo kết nối giữa client và server
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.join(roomId)   //client vafo phong
+    socket.to(roomId).broadcast.emit('user-connected', userId);   //gui tb co client vao phong
     // messages
     socket.on('message', (message) => {
-      //send message to the same room
+      // gửi tin nhắn đến cùng phòng
       io.to(roomId).emit('createMessage', message)
   }); 
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    socket.on('disconnect', () => {      //server lắng nghe dữ liệu từ client ngắt kết nối
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)   //tb client ngat ket noi cko cac thah vien khac
     })
   })
-})
+})    //điều này sẽ chạy bất cứ lúc nào khi ai đó kết nối với trang web của chúng ta,
 
-server.listen(process.env.PORT||3030)
+server.listen(process.env.PORT||3030)     //khai báo PORT mặc định khi chạy(từ hệ thống, nó có PORT nào thì lấy PORT đó)
+
+//req :yeu cau
+// res: Response--tra loi
+// redirect: dieu huong trang den dia chi
